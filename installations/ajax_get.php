8d4757c1adaf8b71d408026577369144efe52fb7
Vittorio Lo Mele <vittorio@mrbackslash.it>
2021-12-24 00:07:23 +0100

    `ajax_get` installations

---
+++ installations/ajax_get.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once '../init.php';
+
+session_start();
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    die('false');
+}
+
+if($_SERVER['REQUEST_METHOD'] != "GET"){
+    die('false');
+}
+
+$id = $con->real_escape_string($_GET['idInstallation']);
+
+$res = $con->query("SELECT * FROM `installations` WHERE `idInstallation` = '$id'");
+
+if($con->affected_rows != 1){
+    die('false');
+}
+
+$arr = $res->fetch_assoc();
+header("Content-Type: application/json");
+
+die(
+    json_encode($arr)
+);
