73d09449cd83476c7f7bc79082112a05dc565356
Vittorio Lo Mele <vittorio@mrbackslash.it>
2022-11-10 10:46:47 +0100

    Update init.php

--- init.php
+++ init.php
@@ -1,10 +1,10 @@
 <?php
 $config = array(
-    "DB_HOST" => "10.8.0.10",
+    "DB_HOST" => "localhost",
     "DB_USER" => "gipcat",
     "DB_PASS" => "gipcat",
     "DB_NAME" => "gipcat",
-    "INST_NAME" => "'Cusenza Service' di G. Cusenza"
+    "INST_NAME" => "Test"
 );
 
 $con = new mysqli($config["DB_HOST"], $config["DB_USER"], $config["DB_PASS"], $config["DB_NAME"]);
